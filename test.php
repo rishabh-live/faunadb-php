b684cc4e717aada2ab6372b0f5afcf1bb0f3c100
rishabh-live <rishabh12536@gmail.com>
2021-04-27 17:07:59 +0530

    CodeRefactor

--- test.php
+++ test.php
@@ -1,10 +1,11 @@
 <?php
 
 require_once 'src/autoload.php';
+require_once './secrets-local.php';
 
 $fauna = new FaunaPhp();
 
-echo $fauna->connectFauna("DamnNice");
+echo $fauna->connectFauna($faunaKey);
 echo "\n";
 echo $fauna->createCollection("Perfect");
 
